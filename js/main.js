d2167b83d8cde4d37b9d45e569157f1ab6e63b4b
Vladimir <Sp1rit911@mail.ru>
2018-11-30 13:25:48 +0300

    Работаю над 3 заданием

---
+++ js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+var calculatePics = function (min, max) {
+  return calculatePics = Math.random() * (max - min) + min;
+};
+var url = calculatePics(1, 25);
+var calculateLikes = function (min, max) {
+  return Math.random() * (max - min) + min;
+};
+calculateLikes()
+var likes = calculateLikes(15, 200);
+var comments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.'];;
+
+var calculateAvatars = function (avatars) {
+  var userAvatar = 0;
+  for ( var i = 1; i <= avatars.length; i++) {
+    userAvatar += avatars[i];
+  }
+  return userAvatar;
+}
+var names = calculateAvatars(1, 25);
+var calculateNames =
+var arrayPictures = [url, likes, comments];
+var pictures =
+
