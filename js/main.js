f3c20b183ddc2e8e6d0e6cf61359f924b5820592
Vladimir <Sp1rit911@mail.ru>
2018-12-03 14:16:14 +0300

    Работаю над 3 модулем

--- js/main.js
+++ js/main.js
@@ -1,15 +1,37 @@
 'use strict';
-var calculatePics = function (min, max) {
-  return calculatePics = Math.random() * (max - min) + min;
+// функция получения какого-то числа
+var toGetNumber = function (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 };
-var url = calculatePics(1, 25);
-var calculateLikes = function (min, max) {
-  return Math.random() * (max - min) + min;
+toGetNumber();
+// какое-то число лайков, переменную приравниваем к рандомному числу//
+var likes = toGetNumber(15, 200);
+// массив имен
+var arrayNames = ['Alex', 'Kate', 'Dadd', 'Todd', 'Mark', 'Jon', 'Vlad', 'Petr', 'Rodd', 'Sten', 'Loki', 'Vasya', 'Petya', 'Ann', 'Sofi', 'Elsa', 'Erl', 'Gray', 'Tedd', 'Jorn', 'Lande', 'Axel', 'Volker', 'Mike', 'John'];
+// подсчёт имен
+var toGetName = function () {
+  var number = toGetNumber(0, arrayNames.length);
+  var names = arrayNames[number];
+  return names;
 };
-calculateLikes()
-var likes = calculateLikes(15, 200);
-var comments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.'];;
-
+var amountNames = 25;
+var namesArray = [];
+for (var i = 0; i <= amountNames; i++) {
+  var name = toGetName();
+  namesArray.push(name);
+}
+// массив комментариев
+var arrayComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.'];
+var calculateComments = toGetNumber(0, 1);
+var comment = arrayComments[calculateComments];
+// Подсчет ссылок
+// var url = 'photos/{{toGetNumber}}.jpg';
+// Массив пользователя
+var urls = toGetNumber(0, 25);
+var url = 'photos/{{urls}}.jpg';
+var arrayUsers = [url, likes, comment];
+var users = Math.random(arrayUsers);
+/*
 var calculateAvatars = function (avatars) {
   var userAvatar = 0;
   for ( var i = 1; i <= avatars.length; i++) {
@@ -17,8 +39,4 @@ var calculateAvatars = function (avatars) {
   }
   return userAvatar;
 }
-var names = calculateAvatars(1, 25);
-var calculateNames =
-var arrayPictures = [url, likes, comments];
-var pictures =
-
+*/
