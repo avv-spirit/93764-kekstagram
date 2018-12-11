0716aca1b1e6f0e4971beb51068c4cd84f8c3117
Vladimir <Sp1rit911@mail.ru>
2018-12-11 17:18:31 +0300

    Модуль 3

--- js/main.js
+++ js/main.js
@@ -1,82 +1,61 @@
 'use strict';
-// Подсчет ссылок
+// 1 - считаем ссылки для фотографии URL, чтобы поставить в url объекта
 var amountNumbers = 25;
-var arrayNumbers = [];
-for (var i = 0; i <= amountNumbers; i++) {
-  i;
-  var photosUrl = 'photos/' + i + '.jpg'
-  arrayNumbers.push(photosUrl)
-};
-// функция получения какого-то числа
+var arrayPhotoNumbers = [];
+for (var i = 1; i <= amountNumbers; i++) {
+  var photosUrl = 'photos/' + i + '.jpg';
+  arrayPhotoNumbers.push(photosUrl);
+}
+
+// 2 - Чтобы получить рандомное число делаем функию с Math.random()
 var toGetNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
-var someLikes = toGetNumber(15, 200); // какое-то число лайков, переменную приравниваем к рандомному числу//
-// массив имен
+
+// 4 - Создаем массив имён, имя будет случайно выбранным
 var arrayNames = ['Alex', 'Kate', 'Dadd', 'Todd', 'Mark', 'Jon', 'Vlad', 'Petr', 'Rodd', 'Sten', 'Loki', 'Vasya', 'Petya', 'Ann', 'Sofi', 'Elsa', 'Erl', 'Gray', 'Tedd', 'Jorn', 'Lande', 'Axel', 'Volker', 'Mike', 'John'];
-var toGetName = function (arrayNames) { // подсчёт имен
+/*
+// 5 - Чтобы получить имя из списка нужно подставить в функцию масссив
+var toGetName = function () {
   var number = toGetNumber(0, arrayNames.length);
   var names = arrayNames[number];
   return names;
 };
-var amountNames = 25;
-var namesArray = [];
-for (var i = 0; i <= amountNames; i++) {
-  var name = toGetName(arrayNames);
-  namesArray.push(name);
-};
-//--------------------
-var arrayComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.']; // Массив комментариев
-//--------------------
-var calculateComments = toGetNumber(0, 2); // Подсчет комментариев
-var comment = arrayComments[calculateComments];
-//--------------------
-//var url = 'photos/' + toGetNumber(0, 24) + '.jpg'; // Подсчет ссылок
+*/
+// 6 - Массив с комментариями
+var arrayComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.'];
+
+// 7 - Считаем аватары, которые потом добавим к объекту comments, в свою очередь потом объект comments подставим в объект objUsers
 var amount = 6;
 var namesAvatars = [];
-for(var i = 0; i <= amount; i++) {
-  i;
-  var avatars = 'img/avatar-' + i + '.svg'; // Подсчет аватаров
+for (var j = 0; j <= amount; j++) {
+  var avatars = 'img/avatar-' + j + '.svg';
   namesAvatars.push(avatars);
-};
-var photosArray = []; //Пустой массив, который мы будем заполнять цифрами
-for(var i = 0; i < amount; i++) { //Задаем цикл нужной длинны.
-  name = toGetNumber(); // Генерируем новое имя и записываем его в переменную name
-  photosArray.push(name); //Записываем сгенерированное имя в массив.
-};
-// объект комментариев
-var commentObject = {
-  avatars: namesAvatars[toGetNumber(0, namesAvatars.length),
-  comments: arrayComments[toGetNumber(0, arrayComments.length),
-  name: namesArray[toGetNumber(0, arrayNames.length)
 }
-// массив для 25 объектов
+// 8 - Объект комментариев, который мы подставим в objUsers к свойству comments:
+var commentObject = {
+  avatars: namesAvatars[toGetNumber(0, namesAvatars.length)],
+  comments: arrayComments[toGetNumber(0, arrayComments.length)],
+  name: arrayNames[toGetNumber(0, arrayNames.length)]
+};
+
+// 9 - Последнее, создаем массив 25 пользователей arrayObjects, в котором будут содержаться объекты пользователей, для этого делаем цикл
+// с пересчетом переменной num, дальше, приравняем её к другой переменной для понятия номера пользователся
 var amountObjects = 25;
 var arrayObjects = [];
-for (var i = 0; i < amountObjects.length; i++) {
-  // переменная с описанием пользователя
-  var usersNames = {
-    url: arrayNumbers[1],
+for (var num = 1; num <= amountObjects; num++) {
+  var users = num;
+  users = {
+    url: arrayPhotoNumbers[toGetNumber(0, arrayPhotoNumbers.length)],
     likes: toGetNumber(15, 200),
     comments: commentObject
   };
-   i = usersNames;
-  usersNames = toGetNumber(0, amountObjects.length);
-  arrayObjects.push(usersNames);
-};
-//--------------------
-//var array = []
-//for
-/*var usersNames = {
-  name: namesArray[toGetNumber(0, arrayNames.length)],
-  avatar: namesAvatars[toGetNumber(0, namesAvatars.length)],
-  url: arrayNumbers[1],
-  likes: toGetNumber(15, 200),
-  comments: arrayComments[toGetNumber(0, arrayComments.length)]
-};
-*/
+  arrayObjects.push(users);
+}
+// console.log()
+/*
 // --- К пункту 4 задания
-/*var bigPicture = document.querySelector('.big-picture');
+var bigPicture = document.querySelector('.big-picture');
 bigPicture.classList.remove('hidden');
 // --- К пункту 5 задания
 var countComments = document.querySelector('.social__comment-count');
@@ -84,4 +63,15 @@ countComments.classList.add('visually-hidden');
 // ---
 var commentsLoader = document.querySelector('.comments-loader');
 commentsLoader.classList.add('visually-hidden');
+
+var amountObjects = 25;
+var arrayObjects = [];
+for (var num = 1; num <= amountObjects; num++) {
+  users = {
+    likes: toGetNumber(15, 200),
+    comments: commentObject
+  };
+  arrayObjects.push(num);
+}
+console.log()
 */
