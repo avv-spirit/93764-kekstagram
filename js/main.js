b33ffaf8dcb4f92753e31ed2efd74c1539baac6b
Vladimir <Sp1rit911@mail.ru>
2018-12-16 10:15:22 +0300

    3 модуль, продолжение

--- js/main.js
+++ js/main.js
@@ -1,58 +1,49 @@
 'use strict';
-// 1 - считаем ссылки для фотографии URL, чтобы поставить в url объекта
-var amountNumbers = 25;
-var arrayPhotoNumbers = [];
-for (var i = 1; i <= amountNumbers; i++) {
-  var photosUrl = 'photos/' + i + '.jpg';
-  arrayPhotoNumbers.push(photosUrl);
-}
-
-// 2 - Чтобы получить рандомное число делаем функию с Math.random()
+var arrayNames = ['Alex', 'Kate', 'Dadd', 'Todd', 'Mark', 'Jon', 'Vlad', 'Petr', 'Rodd', 'Sten', 'Loki', 'Vasya', 'Petya', 'Ann', 'Sofi', 'Elsa', 'Erl', 'Gray', 'Tedd', 'Jorn', 'Lande', 'Axel', 'Volker', 'Mike', 'John'];
+var arrayComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+var calculateUrl = function (amountNumbers) {
+  var arrayPhotoNumbers = [];
+  for (var i = 1; i <= amountNumbers; i++) {
+    var photosUrl = 'photos/' + i + '.jpg';
+    arrayPhotoNumbers.push(photosUrl);
+  }
+  return amountNumbers;
+};
 var toGetNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
-
-// 4 - Создаем массив имён, имя будет случайно выбранным
-var arrayNames = ['Alex', 'Kate', 'Dadd', 'Todd', 'Mark', 'Jon', 'Vlad', 'Petr', 'Rodd', 'Sten', 'Loki', 'Vasya', 'Petya', 'Ann', 'Sofi', 'Elsa', 'Erl', 'Gray', 'Tedd', 'Jorn', 'Lande', 'Axel', 'Volker', 'Mike', 'John'];
-/*
-// 5 - Чтобы получить имя из списка нужно подставить в функцию масссив
-var toGetName = function () {
-  var number = toGetNumber(0, arrayNames.length);
-  var names = arrayNames[number];
-  return names;
+var calculateAvatars = function (amountAvatars) {
+  var namesAvatars = [];
+  for (var j = 0; j <= amountAvatars; j++) {
+    var avatars = 'img/avatar-' + j + '.svg';
+    namesAvatars.push(avatars);
+  }
+  return amountAvatars;
 };
-*/
-// 6 - Массив с комментариями
-var arrayComments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.'];
-
-// 7 - Считаем аватары, которые потом добавим к объекту comments, в свою очередь потом объект comments подставим в объект objUsers
-var amount = 6;
-var namesAvatars = [];
-for (var j = 0; j <= amount; j++) {
-  var avatars = 'img/avatar-' + j + '.svg';
-  namesAvatars.push(avatars);
-}
-// 8 - Объект комментариев, который мы подставим в objUsers к свойству comments:
 var commentObject = {
   avatars: namesAvatars[toGetNumber(0, namesAvatars.length)],
-  comments: arrayComments[toGetNumber(0, arrayComments.length)],
+  comments: arrayComments[toGetNumber(0, 7)],
   name: arrayNames[toGetNumber(0, arrayNames.length)]
 };
+var calculateObjUsers = function (amountObjects) {
+  var arrayObjects = [];
+  for (var num = 1; num <= amountObjects; num++) {
+    var users = num;
+    users = {
+      url: arrayPhotoNumbers[toGetNumber(0, arrayPhotoNumbers.length)],
+      likes: toGetNumber(15, 200),
+      comments: commentObject
+    };
+    arrayObjects.push(users);
+  }
+  return amountObjects;
+};
 
-// 9 - Последнее, создаем массив 25 пользователей arrayObjects, в котором будут содержаться объекты пользователей, для этого делаем цикл
-// с пересчетом переменной num, дальше, приравняем её к другой переменной для понятия номера пользователся
-var amountObjects = 25;
-var arrayObjects = [];
-for (var num = 1; num <= amountObjects; num++) {
-  var users = num;
-  users = {
-    url: arrayPhotoNumbers[toGetNumber(0, arrayPhotoNumbers.length)],
-    likes: toGetNumber(15, 200),
-    comments: commentObject
-  };
-  arrayObjects.push(users);
-}
-// console.log()
+var similarPictures = document.querySelector('.picture');
+var similarPictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture-img');
+document.
 /*
 // --- К пункту 4 задания
 var bigPicture = document.querySelector('.big-picture');
@@ -63,15 +54,4 @@ countComments.classList.add('visually-hidden');
 // ---
 var commentsLoader = document.querySelector('.comments-loader');
 commentsLoader.classList.add('visually-hidden');
-
-var amountObjects = 25;
-var arrayObjects = [];
-for (var num = 1; num <= amountObjects; num++) {
-  users = {
-    likes: toGetNumber(15, 200),
-    comments: commentObject
-  };
-  arrayObjects.push(num);
-}
-console.log()
 */
